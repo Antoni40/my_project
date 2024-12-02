bf7d40e2c6842c70b18ff5a37f3365b2e5810f06
Antoni40 Lubomski <lubomskiantoni24@gmail.com>
2024-12-02 17:52:13 +0000

    create message

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,3 +1,16 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static COUNTER: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk_macros::query]
+fn get() -> String {
+    let a = String::from("123");
+    let b = String::from("3121");
+    COUNTER.with(|counter| (*counter.borrow()).clone())
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
